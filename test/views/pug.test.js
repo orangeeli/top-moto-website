73e9a65fa59186f03c86863ddb801425196c02ec
Eliseu Martinho <eliseu.martinho@gmail.com>
2016-05-13 09:56:01 +0000

    added more test cases for the menu rendering

--- test/views/pug.test.js
+++ test/views/pug.test.js
@@ -8,19 +8,50 @@
     pug = require('pug'),
     cheerio = require('cheerio');
 
-  describe('pug', ()=>{
-
-
-    it("should render menu", ()=>{
-      let html,
-        $;
-
-      html = pug.renderFile(__dirname + '/fixtures/include.menu.mixin.pug');
-
+  describe('pug verkauf page', ()=>{
+    it("should render the menu with 'Verkauf' selected", ()=>{
+      let html, $;
+      html = pug.renderFile(__dirname + '/fixtures/include.menu.mixin.verkauf.pug');
       $ = cheerio.load(html);
-
       assert.isTrue($("div[data-menu-entry='Verkauf']").hasClass("menu-selected"));
+    });
 
+    it("should not render the menu with 'Kontakt' selected", ()=>{
+      let html, $;
+      html = pug.renderFile(__dirname + '/fixtures/include.menu.mixin.verkauf.pug');
+      $ = cheerio.load(html);
+      assert.isFalse($("div[data-menu-entry='Kontakt']").hasClass("menu-selected"));
+    });
+
+    it("should not render the menu with 'Ankauf' selected", ()=>{
+      let html, $;
+      html = pug.renderFile(__dirname + '/fixtures/include.menu.mixin.verkauf.pug');
+      $ = cheerio.load(html);
+      assert.isFalse($("div[data-menu-entry='Ankauf']").hasClass("menu-selected"));
     });
   });
+
+  describe('pug Ankauf page', ()=>{
+    it("should not render the menu with 'Verkauf' selected", ()=>{
+      let html, $;
+      html = pug.renderFile(__dirname + '/fixtures/include.menu.mixin.ankauf.pug');
+      $ = cheerio.load(html);
+      assert.isFalse($("div[data-menu-entry='Verkauf']").hasClass("menu-selected"));
+    });
+
+    it("should not render the menu with 'Kontakt' selected", ()=>{
+      let html, $;
+      html = pug.renderFile(__dirname + '/fixtures/include.menu.mixin.ankauf.pug');
+      $ = cheerio.load(html);
+      assert.isFalse($("div[data-menu-entry='Kontakt']").hasClass("menu-selected"));
+    });
+
+    it("should render the menu with 'Ankauf' selected", ()=>{
+      let html, $;
+      html = pug.renderFile(__dirname + '/fixtures/include.menu.mixin.ankauf.pug');
+      $ = cheerio.load(html);
+      assert.isTrue($("div[data-menu-entry='Ankauf']").hasClass("menu-selected"));
+    });
+  });
+
 })();
